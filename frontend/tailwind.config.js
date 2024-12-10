6a4c079ad3ee15872e33a24a53ccf961c1cba28c
inotbaggi <inotbaggi@gmail.com>
2024-12-10 19:35:51 +0300

    ups

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bg-smooth-t': '#f1ebd6'
+      }
+    },
+  },
+  plugins: [],
+}
