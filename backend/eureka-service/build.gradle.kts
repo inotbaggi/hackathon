542f5938522b334d0f74b9d1345a8a88c9f23a7b
inotbaggi <inotbaggi@gmail.com>
2024-12-10 13:10:55 +0300

    setup eureka

--- backend/eureka-service/build.gradle.kts
+++ backend/eureka-service/build.gradle.kts
@@ -22,9 +22,7 @@ extra["springCloudVersion"] = "2024.0.0"
 
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-	//implementation("org.springframework.cloud:spring-cloud-starter-gateway-mvc")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
